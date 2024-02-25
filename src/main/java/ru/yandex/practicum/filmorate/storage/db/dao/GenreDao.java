cf18b08f571dd560e11ba95ed73cb4532dc554d4
Egor <alexanoffegor@yandex.ru>
2024-02-25 21:03:21 +0300

    11 11

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/dao/GenreDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/dao/GenreDao.java
@@ -5,7 +5,10 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import java.util.List;
 
 public interface GenreDao {
+
     public Genre getById(Integer filmId);
+
     public List<Genre> getAll();
+
     boolean containsInBD(Integer id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/db/dao/LikeDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/dao/LikeDao.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.storage.db.dao;
 
 public interface LikeDao {
+
     void likeOnFilm(Long id, Long userId);
 
     void deleteLikeOnFilm(Long id, Long userId);
